ba1c87d1b835f2caefcda78879c3ac5eb8cd211f
nkhakwan <post_khan@yahoo.com>
2020-04-11 15:25:52 -0700

    this'll push my complete repeat project already completed in the new repo

---
+++ README.md
@@ -0,0 +1,69 @@
+# _Mr Robogar's Neighbourhood_
+
+#### _Epicodus Week 3 Project_
+
+#### By _**KhaanSahab**_
+
+## Description
+
+_This website takes a number and replays a full range of numbers from 0 till the inputted number with any number containing 1,2 or 3 being replaced by funny strings; their are rules for which string is replaced. Numbers could be any number of digits long_
+
+## Setup/Installation Requirements
+
+* Clone this repository.
+* Navigate to index.html file.
+* Right click and open in the browser of your choice.
+
+* Alternatively, open the webpage at [click here](https://nkhakwan.github.io/week3Project/)
+
+## Known Bugs
+
+None so far. Original project was done with not understanding the rules for stings replacement. The original project was completed successfully according to the perceived rules. And that was much difficult to complete than this one. Anyways code related to original project has been commented out but some of it is present in commented form.
+
+## Support and contact details
+
+_Have a bug or an issue with this application? call 999-503-9999_
+
+## Technologies Used
+
+HTML,
+CSS,
+Bootstrap,
+Git and GitHub,
+JavaScript Loops and Arrays and if statements,
+jQuery
+
+### Specs Input
+| Spec | Input | result |
+| :-------------  | :-----------------------------------------------------| :----------------------------------------|
+| **Number input window**  | User Input:"any Number" | Output: "Full range of numbers from 0 to that number with numbers containing 1, 2 and three replaced by funny strings" |
+| **replaced numbers case one** | Input "any number" | Output: "numbers containing one is replaced by BEEP!, numbers containing two are replaced by BOOB! and numbers containing 3 is replaced by "won't you be my neighbour!". Rest remain same|
+| **Case two** | User Input: "numbers containg all or two of 1,2 and 3| Output : All those numbers to be replaced by strings corresponding to most significant of the three numbers present. Example 12 would be replaced by string corresponding to 2. and 13 corresponding to 3 |
+
+### License
+This software is licensed under the Epicodus inc license.
+
+Copyright (c) 2020 **_Khan Sahab_**
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,197 @@
+var mainFunction = function(yourNumber, dominantProgression){
+  var rangeVar = rangeGen(yourNumber);
+  var result = stringInsertor(rangeVar);
+return result; 
+}
+
+
+
+/// ============ rangeGen function =======================================
+
+
+var rangeGen = function(aNumber){
+  var rang = [];
+  for (var i=0; i<(aNumber+1); i=i+1){
+    rang.push(i);
+    var rangg = rang.join();
+  } 
+  return rangg;
+} 
+
+
+
+/// ============== stringInsertor function ============================================
+var stringInsertor = function(justNumbers){
+  var storingArray =[];
+  var splittedNumbers = justNumbers.split(',');
+    for (var i = 0; i < splittedNumbers.length; i=i+1){
+      var singleNumber = splittedNumbers[i];
+      var singleSplitted = singleNumber.split('');
+      var number3Found = false;
+      var number2Found = false;
+      var number1Found = false;
+      var noNumberFound = false;
+        for (var j=0; j<singleSplitted.length ; j= j+1){
+          if (parseInt(singleSplitted[j]) === 3) {
+            number3Found = true;
+          } else if (parseInt(singleSplitted[j]) === 2) {
+            number2Found = true;
+            } else if (parseInt(singleSplitted[j]) === 1) {
+              number1Found = true;
+            } else {
+                noNumberFound =true;
+              }
+        } 
+      if (number3Found){
+        storingArray[i] = "won't you be my neighbour!";
+      } else if(number2Found){
+          storingArray[i] = "Boop";
+        } else if(number1Found){
+            storingArray[i] = "Beep!";
+          } else {
+            storingArray[i] = splittedNumbers[i];
+            } 
+    }
+    return storingArray;
+}
+
+
+//// =========== Jquery section ================================
+
+
+$(document).ready(function() {
+  $("form#inputAndOutput").submit(function(event){
+     event.preventDefault();
+     var yourNumber = parseInt($("#yourNumber").val());
+     var dominantProgression = parseInt($("#dominantProgression").val());
+     var finalResult = mainFunction(yourNumber, dominantProgression);
+     $("#output").text(finalResult);
+     });
+ });
+
+/*var stringInsertor = function(justNumbers){
+  var storingArray =[];
+  var splittedNumbers = justNumbers.split(',');
+  //alert (splittedNumbers);
+    for (var i = 0; i < splittedNumbers.length; i=i+1){
+      //alert(splittedNumbers.length);
+      //var singleNumber = splittedNumbers[i].split(',');
+      var singleNumber = splittedNumbers[i];
+      var singleSplitted = singleNumber.split('');
+     // alert(singleSplitted.length);
+     // alert(singleSplitted);
+      //alert(singleSplitted[0]);
+      //alert(singleSplitted[1]);
+      //alert(single)
+        for (var j=0; j<singleSplitted.length ; j= j+1){
+          //alert(j);
+          //alert(singleSplitted[j]);
+          if (parseInt(singleSplitted[j]) === 1) {
+            storingArray[i] = "Beep!";
+            //alert("first If" + singleSplitted[j]);
+            break;
+          } else if (parseInt(singleSplitted[j]) === 2) {
+            //alert("second");
+            storingArray[i] = "Boop!";
+            break;
+            } else if (parseInt(singleSplitted[j]) === 3) {
+             // alert("third")
+              storingArray[i] = "Won't you be my neighbour!";
+              break
+            } else {
+              //alert("didn't entered");
+                storingArray[i] = splittedNumbers[i];
+              }
+           //alert(storingArray);
+        } //end of second for loop
+          
+    }// end of first for loop
+    return storingArray;
+}// end of function
+
+
+
+//// ============== function lag insertor ===============================
+var lagInsertor = function(justNumbers){
+  var storingArray =[];
+  var splittedNumbers = justNumbers.split(',');
+    for (var i = 0; i < splittedNumbers.length; i=i+1){
+      var singleNumber = splittedNumbers[i];
+      var singleSplitted = singleNumber.split('');
+     // alert(parseInt(singleSplitted[0]));
+      //alert(parseInt(singleSplitted[1]));
+      if ((parseInt(singleSplitted[0]) || parseInt(singleSplitted[1])) === 1 || parseInt((singleSplitted[0]) || parseInt(singleSplitted[1])) === 2 || parseInt((singleSplitted[0]) || parseInt(singleSplitted[1])) === 3){
+        for (var j=0; j<singleSplitted.length ; j= j+1){
+          var tempStorage;
+          if (parseInt(singleSplitted[j]) === 1) {
+            tempStorage = "Beep!";
+          } else if (parseInt(singleSplitted[j]) === 2) {
+            tempStorage = "Boop!";
+            } else if (parseInt(singleSplitted[j]) === 3) {
+              tempStorage = "won't you be my nieghbour!";
+            } else {
+              //alert("didn't entered");
+                var field = field +1;
+              }
+              storingArray[i] = tempStorage;
+        } //end of second for loop
+        
+    } else {
+    storingArray[i] = splittedNumbers[i];
+    }
+    }// end of first for loop 
+    return storingArray;
+}// end of function*/
+
+
+
+
+
+
+/*var stringInsertor = function(justNumbers){
+  alert("i have entered");
+  var storingArray =[];
+  var splittedNumbers = justNumbers.split(',');
+  //alert (splittedNumbers);
+    for (var i = 0; i < splittedNumbers.length; i=i+1){
+      //alert(splittedNumbers.length);
+      //var singleNumber = splittedNumbers[i].split(',');
+      var singleNumber = splittedNumbers[i];
+      var singleSplitted = singleNumber.split('');
+      //alert(singleSplitted);
+      var number3Found = false;
+      var number2Found = false;
+      var number1Found = false;
+      var noNumberFound = false;
+      singleSplitted.forEach(function(eachNumber){
+        if (eachNumber === 3){
+          number3Found = true;
+        } else if (eachNumber === 2){  
+          number2Found = true;
+          } else if (eachNumber === 1){
+            number1Found = true;
+            } else {
+            noNumberFound = true;
+              }
+      });
+      alert(number3Found);
+      if (number3Found){
+        storingArray[i] = "won't you be my neighbour!";
+      } else if(number2Found){
+          storingArray[i] = "Boop";
+        } else if(number1Found){
+            storingArray[i] = "Beep!";
+          } else {
+            storingArray[i] = splittedNumbers[i];
+            } 
+    }// end of first for loop
+    return storingArray;
+}// end of function*/
+
+
+
+
+
+
+
+
